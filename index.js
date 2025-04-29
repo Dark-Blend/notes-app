7ac2c8878c47414244b7de9632c4edf165deb67a
Dark-Blend <blendedintodarkness@gmail.com>
2025-04-29 12:01:04 +0530

    edited git ignore and added files

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   fs.readdir("./files", (err, files) => {
